1b76f9927a0374d8d291fa2cfb6133b0982eafe7
Mohammed <moh.jameel178@gmail.com>
2019-02-06 16:56:30 +0300

    Just Practic

--- examples/main.js
+++ examples/main.js
@@ -1,5 +1,15 @@
 // We loaded axios library in our html file
 // Lets make an ajax call with axios
 
+function addToScreen(response) {
+    const img = document.createElement('img');
+    img.src = response.data.message;
+    document.body.appendChild(img);
+}
+axios({
+    method: 'get',
+    url: 'https://dog.ceo/api/breed/boxer/images/random'
+})
+    .then(addToScreen);
 // method: get
 // url: https://dog.ceo/api/breed/boxer/images/random
